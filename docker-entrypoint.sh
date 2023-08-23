7401aa5b3381f3a2431c75ab4083b78dea1dd5b1
joramk <joramk@gmail.com>
2023-08-23 18:16:44 +0200

    Added use of TZ env var

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 /usr/libexec/httpd-ssl-gencerts
+if [ ! -z "$TZ" ]; then
+        if [ -e "/usr/share/zoneinfo/$TZ" ]; then
+                ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime
+                sed -i "s|;date.timezone =|date.timezone = $TZ|g" /etc/php.ini
+        fi
+fi
 exec "$@"
 
